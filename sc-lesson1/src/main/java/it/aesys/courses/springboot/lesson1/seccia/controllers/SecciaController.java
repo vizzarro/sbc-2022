d21b6ba53fd928cb95264209cc4029320a8c165e
AntoS3 <antonio.seccia@aesys.tech>
2022-11-11 14:00:59 +0100

    seccia commit

---
+++ sc-lesson1/src/main/java/it/aesys/courses/springboot/lesson1/seccia/controllers/SecciaController.java
@@ -0,0 +1,34 @@
+package it.aesys.courses.springboot.lesson1.seccia.controllers;
+
+import it.aesys.courses.springboot.lesson1.config.SecciaConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("seccia")
+public class SecciaController {
+
+
+    private SecciaConfig config;
+
+    @Autowired
+    public SecciaController(SecciaConfig config) {
+        super();
+        this.config = config;
+    }
+
+
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String lesson() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("<H1>SPRING BOOT "+config.getAppName()+" </H1>");
+        builder.append("<H2>"+config.getDescription()+"</H2>");
+        builder.append("<H3>Version : "+config.getVersion()+"</H3>");
+        return builder.toString();
+    }
+
+}
