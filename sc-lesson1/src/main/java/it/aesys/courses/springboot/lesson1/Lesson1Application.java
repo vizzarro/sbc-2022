b855c13b2e5fbe7dcadbd7467ce26b564f8d6072
Aesys <caterina.derosa@aesys.tech>
2022-11-11 12:20:44 +0100

    fix di caterina

--- sc-lesson1/src/main/java/it/aesys/courses/springboot/lesson1/Lesson1Application.java
+++ sc-lesson1/src/main/java/it/aesys/courses/springboot/lesson1/Lesson1Application.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,18 +18,19 @@ import java.util.Arrays;
 @RestController
 @RequestMapping(path = "/")
 @SpringBootApplication
+@ConfigurationPropertiesScan("it.aesys.courses.springboot.lesson1.config")
 public class Lesson1Application {
 
 	//private static final Logger logger = LoggerFactory.getLogger(Lesson1Application.class);
 	
 	@Value("${app.lesson}")
-	private String lessonNumber = "1";
+	private String lessonNumber;
 	
 	@Value("${lesson.welcome-message}")
 	private String lessonMessage;
 
 	@Value("${lesson.author}")
-	private String lessonAuthor = "Alessandro";
+	private String lessonAuthor;
 
 	private ConfigService configService;
 	
@@ -42,7 +44,6 @@ public class Lesson1Application {
 		StringBuilder builder = new StringBuilder();
 		builder.append("<H1>SPRING BOOT "+lessonNumber+" </H1>");
 		builder.append("<H2>"+lessonAuthor+"</H2>");
-		builder.append("<H3>"+lessonMessage+"</H3>");
 		return builder.toString();
 	}
 
