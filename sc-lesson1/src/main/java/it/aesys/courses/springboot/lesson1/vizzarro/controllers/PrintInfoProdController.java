92719d0a0151500fa5a581220630fb74bedd9dc9
vizzarro <43704449+vizzarro@users.noreply.github.com>
2022-11-11 14:57:14 +0100

    Add Profiles Component Implementation

--- sc-lesson1/src/main/java/it/aesys/courses/springboot/lesson1/vizzarro/controllers/PrintInfoProdController.java
+++ sc-lesson1/src/main/java/it/aesys/courses/springboot/lesson1/vizzarro/controllers/PrintInfoProdController.java
@@ -9,14 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("vizzarro")
-@Profile("devel")
-public class PrintInfoController {
+@Profile("production")
+public class PrintInfoProdController {
 
 
     private VizConfig config;
 
     @Autowired
-    public PrintInfoController(VizConfig config) {
+    public PrintInfoProdController(VizConfig config) {
         super();
         this.config = config;
     }
@@ -27,8 +27,9 @@ public class PrintInfoController {
     @RequestMapping(method = RequestMethod.GET)
     public String lesson() {
         StringBuilder builder = new StringBuilder();
-        builder.append("<H1>HELLO WORLD !!!</H1>");
+        builder.append("<H1>SPRING BOOT "+config.getAppName()+" </H1>");
+        builder.append("<H2>"+config.getDescription()+"</H2>");
+        builder.append("<H3>Version : "+config.getVersion()+"</H3>");
         return builder.toString();
     }
-
 }
