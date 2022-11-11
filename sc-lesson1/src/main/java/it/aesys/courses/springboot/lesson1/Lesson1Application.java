b89b2221c0d9b3bcfe6ca91283853f1dee7f2d23
gabriele <gabriele.dirocco@aesys.tech>
2022-11-11 12:22:05 +0100

    dirocco commit

--- sc-lesson1/src/main/java/it/aesys/courses/springboot/lesson1/Lesson1Application.java
+++ sc-lesson1/src/main/java/it/aesys/courses/springboot/lesson1/Lesson1Application.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,21 +18,18 @@ import java.util.Arrays;
 @RestController
 @RequestMapping(path = "/")
 @SpringBootApplication
+@ConfigurationPropertiesScan("it.aesys.courses.springboot.lesson1.config")
 public class Lesson1Application {
 
 	//private static final Logger logger = LoggerFactory.getLogger(Lesson1Application.class);
-	
 	@Value("${app.lesson}")
 	private String lessonNumber = "1";
-	
 	@Value("${lesson.welcome-message}")
 	private String lessonMessage;
-
 	@Value("${lesson.author}")
 	private String lessonAuthor = "Alessandro";
 
 	private ConfigService configService;
-	
 	public static void main(String[] args) {
 
 		SpringApplication.run(Lesson1Application.class, args);
@@ -45,8 +43,5 @@ public class Lesson1Application {
 		builder.append("<H3>"+lessonMessage+"</H3>");
 		return builder.toString();
 	}
-
-
-
 
 }
