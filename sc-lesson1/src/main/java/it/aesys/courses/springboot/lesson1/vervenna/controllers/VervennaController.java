a40defb9aa2f0f6ceafbfa3307351ff922c6f997
DESKTOP-UL3AA9U\Aesys <giacomo.vervenna@aesys.tech>
2022-11-11 14:14:55 +0100

    vervenna's commit

---
+++ sc-lesson1/src/main/java/it/aesys/courses/springboot/lesson1/vervenna/controllers/VervennaController.java
@@ -0,0 +1,28 @@
+package it.aesys.courses.springboot.lesson1.vervenna.controllers;
+
+import it.aesys.courses.springboot.lesson1.config.VervennaConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/vervenna")
+public class VervennaController {
+
+    private VervennaConfig config;
+
+    @Autowired
+    public VervennaController(VervennaConfig config){
+        this.config = config;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String lesson() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("<H1>SPRING BOOT "+config.getAppName()+" </H1>");
+        builder.append("<H2>"+config.getDescription()+"</H2>");
+        builder.append("<H3>"+config.getVersion()+"</H3>");
+        return builder.toString();
+    }
+}
