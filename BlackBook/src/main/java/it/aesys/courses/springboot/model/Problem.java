ceec58a7afcebf0f59d537c78a651701ff96ba6e
LAPTOP-JK924VS8\admin <francesca.dicesare@aesys.tech>
2022-11-14 15:55:36 +0100

    commit fraa

--- BlackBook/src/main/java/it/aesys/courses/springboot/model/Problem.java
+++ BlackBook/src/main/java/it/aesys/courses/springboot/model/Problem.java
@@ -1,4 +1,17 @@
 package it.aesys.courses.springboot.model;
 
 public class Problem {
+
+    private String problemType;
+
+    public Problem(){
+
+    }
+    public void setProblemType(String problemType) {
+        this.problemType = problemType;
+    }
+
+    public String getProblemType() {
+        return problemType;
+    }
 }
