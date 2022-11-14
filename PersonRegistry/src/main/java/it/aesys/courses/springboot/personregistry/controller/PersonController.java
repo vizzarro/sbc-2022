3a73a855fef87191b18b633d1af7715910d87a86
Luca Brandolini <lucabrandolini@gmx.com>
2022-11-14 21:25:45 +0100

    aggiunta create nel controller e PersonRequest

--- PersonRegistry/src/main/java/it/aesys/courses/springboot/personregistry/controller/PersonController.java
+++ PersonRegistry/src/main/java/it/aesys/courses/springboot/personregistry/controller/PersonController.java
@@ -1,14 +1,35 @@
 package it.aesys.courses.springboot.personregistry.controller;
 
+import it.aesys.courses.springboot.personregistry.models.PersonDTO;
+import it.aesys.courses.springboot.personregistry.service.PersonService;
+import it.aesys.courses.springboot.personregistry.service.exceptions.ServiceException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping
+@RequestMapping("personregistry")
 //@Profile("")
 public class PersonController {
 
-  //  private PersonService service;
+
+
+    private PersonService service;
+
+    @Autowired
+    public PersonController(PersonService service){
+        this.service = service;
+    }
+
+    @PostMapping
+    public PersonDTO addPerson(@RequestBody request.PersonRequest personRequest) throws ServiceException {
+
+        return service.create(personRequest.getPersonDTO());
+    }
+
+
 
 }
