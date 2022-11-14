7fc11842f158ebc50f751fdd3fede68bd76f3914
LAPTOP-TUGE21QO\Luca Brandolini <luca.brandolini@aesys.tech>
2022-11-14 16:05:31 +0100

    commit prima parte dao

---
+++ PersonRegistry/src/main/java/it/aesys/courses/springboot/personregistry/repository/exception/ComponentException.java
@@ -0,0 +1,24 @@
+package it.aesys.courses.springboot.personregistry.repository.exception;
+
+public class ComponentException extends Exception{
+
+
+    public ComponentException() {
+    }
+
+    public ComponentException(String message) {
+        super(message);
+    }
+
+    public ComponentException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ComponentException(Throwable cause) {
+        super(cause);
+    }
+
+    public ComponentException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
