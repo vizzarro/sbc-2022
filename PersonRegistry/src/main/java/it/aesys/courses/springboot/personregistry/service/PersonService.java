4aa22d7898331b65ac689b32b59f7fbd54ffffdc
LAPTOP-TUGE21QO\Luca Brandolini <luca.brandolini@aesys.tech>
2022-11-14 18:11:32 +0100

    Commit servicePerson e exception

--- PersonRegistry/src/main/java/it/aesys/courses/springboot/personregistry/service/PersonService.java
+++ PersonRegistry/src/main/java/it/aesys/courses/springboot/personregistry/service/PersonService.java
@@ -1,4 +1,31 @@
-package service;
+package it.aesys.courses.springboot.personregistry.service;
+
+import it.aesys.courses.springboot.personregistry.repository.PersonDao;
+import it.aesys.courses.springboot.personregistry.service.exceptions.ServiceException;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class PersonService {
+
+    private PersonMapperDTO personMapperDTO;
+    private PersonDao personDao;
+
+
+    @Autowired
+    public PersonService(PersonMapperDTO personMapperDTO, PersonDao personDao){
+        this.personMapperDTO = personMapperDTO;
+        this.personDao = personDao;
+    }
+
+    public PersonMapperDTO create(PersonMapperDTO personMapperDTO) throws ServiceException {
+
+        try {
+            return personMapperDTO.toDto(personDao.create(personMapperDTO.toModel(personDto)));
+        }
+        catch (ComponentException e){
+            ServiceException ex = new ServiceException();
+            ex.setStatusCode(e.getStatusCode());
+            throw ex;
+        }
+    }
+
 }
--- PersonRegistry/src/main/java/it/aesys/courses/springboot/personregistry/service/exceptions/ServiceException.java
+++ PersonRegistry/src/main/java/it/aesys/courses/springboot/personregistry/service/exceptions/ServiceException.java
@@ -1,4 +1,33 @@
 package it.aesys.courses.springboot.personregistry.service.exceptions;
 
 public class ServiceException extends Exception{
+
+    int statusCode = 0;
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public ServiceException() {
+    }
+
+    public ServiceException(String message) {
+        super(message);
+    }
+
+    public ServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ServiceException(Throwable cause) {
+        super(cause);
+    }
+
+    public ServiceException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
