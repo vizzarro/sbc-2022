b67af2d7a15c512a55e4dfa302191386aef194e9
AlessioDLorito <alessio.dilorito@aesys.tech>
2022-11-14 15:35:42 +0100

    Di Lorito Commit

---
+++ PersonRegistry/src/main/java/models/EnumAddress.java
@@ -0,0 +1,10 @@
+package models;
+
+public class EnumAddress {
+    public static final String RESIDENCE = "Residence";
+    public static final String DOMICILE = "Domicile" ;
+
+    public EnumAddress Address;{
+        RESIDENCE, DOMICILE;
+    }
+}
