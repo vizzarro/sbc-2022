aa0a477e145de3de59317c365f0a499331c77f42
Matteo Golini <matteo.golini@aesys.tech>
2022-11-14 16:42:53 +0100

    CriminalRecord Interface

--- BlackBook/src/main/java/it/aesys/courses/springboot/service/impl/BlackBookServiceImpl.java
+++ BlackBook/src/main/java/it/aesys/courses/springboot/service/impl/BlackBookServiceImpl.java
@@ -1,8 +1,24 @@
 package it.aesys.courses.springboot.service.impl;
 
+import it.aesys.courses.springboot.model.CriminalRecord;
 import it.aesys.courses.springboot.service.BlackBookService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BlackBookServiceImpl implements BlackBookService {
+
+    @Override
+    public CriminalRecord getCriminalRecord(String fiscalCodeNumber) {
+        return null;
+    }
+
+    @Override
+    public CriminalRecord addCriminalRecord (CriminalRecord newCriminalRecord) {
+        return null;
+    }
+
+    @Override
+    public CriminalRecord updateCriminalRecord(String fiscalCodeNumber, CriminalRecord updated) {
+        return null;
+    }
 }
