47503fccc11cbeedfe7396c622ccca0fd8d406eb
AntoS3 <antonio.seccia@aesys.tech>
2022-11-14 16:50:29 +0100

    complete commit/push

--- BlackBook/src/main/java/it/aesys/courses/springboot/service/CriminalRecordService.java
+++ BlackBook/src/main/java/it/aesys/courses/springboot/service/CriminalRecordService.java
@@ -2,7 +2,7 @@ package it.aesys.courses.springboot.service;
 
 import it.aesys.courses.springboot.model.CriminalRecord;
 
-public interface BlackBookService {
+public interface CriminalRecordService {
 
     CriminalRecord getCriminalRecord (String fiscalCodeNumber);
     CriminalRecord addCriminalRecord(CriminalRecord newCriminalRecord);
