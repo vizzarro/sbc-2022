3d19b0e91ae2dfebd73b64cdd582973ba4b0e7ed
ValerioMaiolini <valerio.maiolini@aesys.tech>
2022-11-15 17:53:03 +0100

    Error response + Get by CF fixed(2)

--- DocumentsRepo/src/main/java/it/aesys/courses/springboot/exception/handler/ExceptionManager.java
+++ DocumentsRepo/src/main/java/it/aesys/courses/springboot/exception/handler/ExceptionManager.java
@@ -5,6 +5,7 @@ import it.aesys.courses.springboot.exception.InvalidInputException;
 import it.aesys.courses.springboot.exception.NotFoundException;
 import it.aesys.courses.springboot.models.dto.ErrorResponse;
 import org.springframework.beans.TypeMismatchException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,7 +15,7 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class ExceptionManager {
 
-    @ExceptionHandler({NotFoundException.class, AlreadyExistsException.class, InvalidInputException.class, TypeMismatchException.class})
+    @ExceptionHandler({AlreadyExistsException.class, InvalidInputException.class, TypeMismatchException.class})
     public ResponseEntity<ErrorResponse> handleBadRequest(Exception e){
         ErrorResponse response = new ErrorResponse();
         response.setError("Bad Request");
@@ -23,6 +24,17 @@ public class ExceptionManager {
         response.setTime(LocalDateTime.now());
         return ResponseEntity.badRequest().body(response);
     }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFound(Exception e){
+        ErrorResponse response = new ErrorResponse();
+        response.setError("Not Found");
+        response.setStatus(404);
+        response.setMessage(e.getMessage());
+        response.setTime(LocalDateTime.now());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
 
 
 }
