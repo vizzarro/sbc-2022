cc4e95bf858da4ea5b28be3593b1f17426957fde
ValerioMaiolini <valerio.maiolini@aesys.tech>
2022-11-15 09:48:47 +0100

    createDocument service and controller

--- DocumentsRepo/src/main/java/it/aesys/courses/springboot/controllers/DocumentController.java
+++ DocumentsRepo/src/main/java/it/aesys/courses/springboot/controllers/DocumentController.java
@@ -1,6 +1,29 @@
 package it.aesys.courses.springboot.controllers;
 
+import it.aesys.courses.springboot.models.Document;
+import it.aesys.courses.springboot.models.dto.DocumentRequest;
+import it.aesys.courses.springboot.services.DocumentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/document")
 public class DocumentController {
+
+    DocumentService service;
+
+    @Autowired
+    public DocumentController(DocumentService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Document createDocument(@RequestBody DocumentRequest request) {
+        return service.createDocument(request);
+    }
 
 
 
