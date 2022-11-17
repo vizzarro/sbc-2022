34f6050339dd572db9e049966b07c4e9660ea057
DESKTOP-UL3AA9U\Aesys <giacomo.vervenna@aesys.tech>
2022-11-17 09:48:19 +0100

    Dao interface, Dao Iterface extended

---
+++ PersonRegistry/src/main/java/it/aesys/courses/springboot/personregistry/dao/AddressDao.java
@@ -0,0 +1,6 @@
+package it.aesys.courses.springboot.personregistry.dao;
+
+import it.aesys.courses.springboot.personregistry.models.Address;
+
+public interface AddressDao extends Dao<Address> {
+}
--- PersonRegistry/src/main/java/it/aesys/courses/springboot/personregistry/dao/Dao.java
+++ PersonRegistry/src/main/java/it/aesys/courses/springboot/personregistry/dao/Dao.java
@@ -1,10 +1,12 @@
 package it.aesys.courses.springboot.personregistry.dao;
 
+import java.util.List;
+
 public interface Dao<T> {
 
-    <T> create(<T>);
-    <T> update(<T>);
-    void delete(String);
-    <T> get(String);
-    <List<T>> getAll();
+    T create(T t);
+    T update(T t) ;
+    void delete(String s);
+    T get(String s);
+    List<T> getAll();
 }
