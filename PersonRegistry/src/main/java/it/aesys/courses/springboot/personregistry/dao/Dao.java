167c1f07a71f56749cad57753565b040b4518493
DESKTOP-UL3AA9U\Aesys <giacomo.vervenna@aesys.tech>
2022-11-17 09:30:34 +0100

    Dao interface

---
+++ PersonRegistry/src/main/java/it/aesys/courses/springboot/personregistry/dao/Dao.java
@@ -0,0 +1,10 @@
+package it.aesys.courses.springboot.personregistry.dao;
+
+public interface Dao<T> {
+
+    <T> create(<T>);
+    <T> update(<T>);
+    void delete(String);
+    <T> get(String);
+    <List<T> getAll();
+}
