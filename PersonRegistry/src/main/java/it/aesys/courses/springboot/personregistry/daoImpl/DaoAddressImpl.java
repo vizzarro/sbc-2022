463256d16dfb7407fa6141ddff7b81a951653b28
LAPTOP-TUGE21QO\Luca Brandolini <luca.brandolini@aesys.tech>
2022-11-17 12:16:33 +0100

    DaoImpl e DaoAddressImpl, quasi funzionanti

---
+++ PersonRegistry/src/main/java/it/aesys/courses/springboot/personregistry/daoImpl/DaoAddressImpl.java
@@ -0,0 +1,93 @@
+package it.aesys.courses.springboot.personregistry.daoImpl;
+
+import it.aesys.courses.springboot.personregistry.models.Address;
+import it.aesys.courses.springboot.personregistry.models.EnumAddress;
+import it.aesys.courses.springboot.personregistry.models.Person;
+
+import java.sql.*;
+
+public class DaoAddressImpl {
+
+
+        String dbURL = "jdbc:mysql://localhost:3306/personregistrydb";
+        String username = "root";
+        String password = "secret";
+
+        private static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+
+
+    private static final String INSERT_ADDRESS_SQL = "INSERT INTO address" +
+                "  ( street, civic, postalCode, home ) VALUES " +
+                " ( ?, ?, ?, ? );";
+    private static final String GET_ADDRESS_SQL = "SELECT * FROM persons WHERE address_ID = " +
+            "  ( street, civic, postalCode, home ) VALUES " +
+            " ( ?, ?, ?, ? );";
+
+
+        public void insertAddress(Address address) throws SQLException {
+            System.out.println(INSERT_ADDRESS_SQL);
+            // Step 1: Establishing a Connection
+            try (Class.forName(DRIVER_NAME);
+                 Connection connection = DriverManager
+                         .getConnection(dbURL, username, password);
+
+                 // Step 2:Create a statement using connection object
+                 PreparedStatement preparedStatement = connection.prepareStatement(INSERT_ADDRESS_SQL)) {
+                preparedStatement.setString(1, address.getStreet());
+                preparedStatement.setString(2, address.getCivic());
+                preparedStatement.setInt(3, address.getPostalCode());
+                preparedStatement.setString(4, address.getHome().name());
+
+                System.out.println(preparedStatement);
+                // Step 3: Execute the query or update query
+                preparedStatement.executeUpdate();
+            } catch (SQLException e) {
+
+                // print SQL exception information
+                printSQLException(e);
+            }
+
+            // Step 4: try-with-resource statement will auto close the connection.
+        }
+
+    public Person getAddress(Int address_Id) throws SQLException {
+        System.out.println(GET_ADDRESS_SQL);
+        // Step 1: Establishing a Connection
+        try (Connection connection = DriverManager
+                .getConnection("jdbc:mysql://localhost:3306/mysql_database?useSSL=false", "root", "root");
+
+
+             // Step 2:Create a statement using connection object
+             PreparedStatement preparedStatement = connection.prepareStatement(GET_PERSONS_SQL)) {
+
+
+            System.out.println(preparedStatement);
+            // Step 3: Execute the query or update query
+
+            ResultSet rs = preparedStatement.executeQuery();
+
+        } catch (SQLException e) {
+
+            // print SQL exception information
+            printSQLException(e);
+        }
+
+
+
+        public static void printSQLException(SQLException ex) {
+            for (Throwable e: ex) {
+                if (e instanceof SQLException) {
+                    e.printStackTrace(System.err);
+                    System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+                    System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+                    System.err.println("Message: " + e.getMessage());
+                    Throwable t = ex.getCause();
+                    while (t != null) {
+                        System.out.println("Cause: " + t);
+                        t = t.getCause();
+                    }
+                }
+            }
+        }
+
+    }
--- PersonRegistry/src/main/java/it/aesys/courses/springboot/personregistry/daoImpl/DaoImpl.java
+++ PersonRegistry/src/main/java/it/aesys/courses/springboot/personregistry/daoImpl/DaoImpl.java
@@ -1,79 +1,103 @@
 package it.aesys.courses.springboot.personregistry.daoImpl;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.PreparedStatement;
+import it.aesys.courses.springboot.personregistry.models.Person;
+
+import java.sql.*;
 
 public class DaoImpl {
 
-    String dbURL = "jdbc:mysql://localhost:3306/personregistrydb";
-    String username = "root";
-    String password = "secret";
+    String dbURL = "jdbc:mysql://192.168.130.6:3306/";
+    String username = "user_library";
+    String password = "password";
+    private static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
 
     private static final String INSERT_PERSONS_SQL = "INSERT INTO persons" +
-            "  (id, name, surname, fiscalcode, gender, address, birthdate, cellnumber, ) VALUES " +
-            " (?, ?, ?, ?, ? , ? , ? , ?);";
+            "  ( name, surname, fiscalcode, gender, address, birthdate, cellnumber, ) VALUES " +
+            " ( ?, ?, ?, ? , ? , ? , ?);";
+    private static final String GET_PERSONS_SQL = "SELECT fiscalcode FROM persons WHERE fiscalcode =  ?";
 
-//try { (Connection conn = DriverManager.getConnection(dbURL, username, password))
-//
-//
-//        if (conn != null) {
-//            System.out.println("Connected");
-//        }
-//    } catch (
-//    SQLException ex) {
-//        ex.printStackTrace();
-//    }
-//        }
+    public void insertPerson(Person person) throws SQLException {
 
-        public void insertPerson() throws SQLException {
-            System.out.println(INSERT_PERSONS_SQL);
-            // Step 1: Establishing a Connection
-            try (Connection connection = DriverManager
-                    .getConnection("jdbc:mysql://localhost:3306/mysql_database?useSSL=false", "root", "root");
+        System.out.println(INSERT_PERSONS_SQL);
+        // Step 1: Establishing a Connection
+        try {
+            Class.forName(DRIVER_NAME);
+            Connection connection = DriverManager
+                    .getConnection(dbURL, username, password);
 
-                 // Step 2:Create a statement using connection object
-                 PreparedStatement preparedStatement = connection.prepareStatement(INSERT_PERSONS_SQL)) {
-                preparedStatement.setInt(1, 1);
-                preparedStatement.setString(2, "Tony");
-                preparedStatement.setString(3, "tony@gmail.com");
-                preparedStatement.setString(4, "US");
-                preparedStatement.setString(5, "secret");
+            // Step 2:Create a statement using connection object
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_PERSONS_SQL));
+            preparedStatement.setString(1, person.getName());
+            preparedStatement.setString(2, person.getSurname());
+            preparedStatement.setString(3, person.getFiscalCode());
+            preparedStatement.setString(4, person.getGender().name());
+            preparedStatement.setDate(6, (Date) person.getBirthDate());
+            preparedStatement.setString(7, person.getCellNumber());
 
-                System.out.println(preparedStatement);
-                // Step 3: Execute the query or update query
-                preparedStatement.executeUpdate();
-            } catch (SQLException e) {
+            System.out.println(preparedStatement);
+            // Step 3: Execute the query or update query
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            // print SQL exception information
+            printSQLException(e);
+        } catch (ClassNotFoundException ex) {
 
-                // print SQL exception information
-                printSQLException(e);
-            }
-
-            // Step 4: try-with-resource statement will auto close the connection.
         }
 
-        public static void printSQLException(SQLException ex) {
-            for (Throwable e: ex) {
-                if (e instanceof SQLException) {
-                    e.printStackTrace(System.err);
-                    System.err.println("SQLState: " + ((SQLException) e).getSQLState());
-                    System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
-                    System.err.println("Message: " + e.getMessage());
-                    Throwable t = ex.getCause();
-                    while (t != null) {
-                        System.out.println("Cause: " + t);
-                        t = t.getCause();
-                    }
+        // Step 4: try-with-resource statement will auto close the connection.
+    }
+
+    public static void printSQLException(SQLException ex) {
+        for (Throwable e : ex) {
+            if (e instanceof SQLException) {
+                e.printStackTrace(System.err);
+                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+                System.err.println("Message: " + e.getMessage());
+                Throwable t = ex.getCause();
+                while (t != null) {
+                    System.out.println("Cause: " + t);
+                    t = t.getCause();
                 }
             }
         }
     }
 
+    public Person getPerson(String fiscalcode) throws SQLException {
+        Person p = null;
+        System.out.println(GET_PERSONS_SQL);
+        // Step 1: Establishing a Connection
+        try (Connection connection = DriverManager
+                .getConnection("jdbc:mysql://localhost:3306/mysql_database?useSSL=false", "root", "root")) {
 
 
+            // Step 2:Create a statement using connection object
+            PreparedStatement preparedStatement = connection.prepareStatement(GET_PERSONS_SQL);
+            preparedStatement.setString(1, fiscalcode);
 
+            System.out.println(preparedStatement);
+            // Step 3: Execute the query or update query
 
+            ResultSet rs = preparedStatement.executeQuery();
 
+            //TODO ciclare il rs per prendere i campi e settarlo nella entity
+            while (rs.next()) {
+                p.setName();
+                p.setSurname();
+                p.setFiscalCode();
+                p.setGender();
+                p.setAddress();
+                p.setBirthDate();
+                p.getCellNumber();
 
+            }
+
+        } catch (SQLException e) {
+
+            // print SQL exception information
+            printSQLException(e);
+        }
+        return p;
+
+    }
 }
