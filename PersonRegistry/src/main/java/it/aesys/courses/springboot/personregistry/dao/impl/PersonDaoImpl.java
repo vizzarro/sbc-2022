a1fcb8fa7650ec3126b8d7f1b6a784bb7d4381e4
LucaCrocetta <luca.crocetta@aesys.tech>
2022-11-17 15:00:35 +0100

    crocetta persondaoimpl get all

--- PersonRegistry/src/main/java/it/aesys/courses/springboot/personregistry/dao/impl/PersonDaoImpl.java
+++ PersonRegistry/src/main/java/it/aesys/courses/springboot/personregistry/dao/impl/PersonDaoImpl.java
@@ -5,6 +5,7 @@ import it.aesys.courses.springboot.personregistry.models.EnumGender;
 import it.aesys.courses.springboot.personregistry.models.Person;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PersonDaoImpl implements PersonDao {
@@ -19,6 +20,7 @@ public class PersonDaoImpl implements PersonDao {
             " ( ?, ?, ?, ? , ? , ? , ?);";
     private static final String GET_PERSONS_SQL = "SELECT fiscalcode FROM persons WHERE fiscalcode =  ?";
 
+    private static final String GET_ALL_SQL = "SELECT fiscalcode FROM persons";
     @Override
     public Person create(Person person) //throws SQLException
     {
@@ -129,6 +131,42 @@ public class PersonDaoImpl implements PersonDao {
 
     @Override
     public List<Person> getAll() {
-        return null;
+            List<Person> personList = new ArrayList<>();
+
+            // Step 1: Establishing a Connection
+            try {
+                Class.forName(DRIVER_NAME);
+                Connection connection = DriverManager
+                        .getConnection(dbURL, username, password);
+
+
+                // Step 2:Create a statement using connection object
+                Statement statement = connection.createStatement();
+
+
+                // Step 3: Execute the query or update query
+
+                ResultSet rs = statement.executeQuery(GET_ALL_SQL);
+
+                //TODO ciclare il rs per prendere i campi e settarlo nella entity
+                while (rs.next()) {
+                    Person p = new Person();
+                    p.setName(rs.getString("name"));
+                    p.setSurname(rs.getString("surname"));
+                    p.setFiscalCode(rs.getString("fiscalcode"));
+                    p.setGender(EnumGender.valueOf(rs.getString("gender")));
+                    p.setBirthDate(rs.getDate("birth_date"));
+                    p.setCellNumber(rs.getString("cellnumber"));
+                    personList.add(p);
+                }
+
+            } catch (SQLException e) {
+
+                // print SQL exception information
+                printSQLException(e);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            return personList;
     }
 }
