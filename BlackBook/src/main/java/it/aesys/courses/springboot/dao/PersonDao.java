d92e3ad2e7258acf3922f653ccb49f21046237a4
Matteo Golini <matteo.golini@aesys.tech>
2022-11-17 11:07:28 +0100

    prepared statement second

--- BlackBook/src/main/java/it/aesys/courses/springboot/dao/PersonDao.java
+++ BlackBook/src/main/java/it/aesys/courses/springboot/dao/PersonDao.java
@@ -1,4 +1,12 @@
 package it.aesys.courses.springboot.dao;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class PersonDao {
+    private static final String DRIVER_NAME = " da mettere ";
+    private static final String SQLSelect = "SELECT * FROM person WHERE fiscalCodeNumber=?";
+    private static final String SQLInsert = "INSERT INTO person (fiscalCodeNumber) VALUE('?')";
+    private static final String SQLDelete = "DELETE FROM person WHERE fiscalCodeNumber='?'";
+
 }
--- BlackBook/src/main/java/it/aesys/courses/springboot/dao/ReportDao.java
+++ BlackBook/src/main/java/it/aesys/courses/springboot/dao/ReportDao.java
@@ -6,9 +6,9 @@ import java.sql.*;
 public class ReportDao{
         private static final String DRIVER_NAME = " da mettere ";
         private static final String SQLSelect = "SELECT * FROM report WHERE reportTicketNumber=?";
-        private static final String SQLInsert = "INSERT INTO report (reportTickerNumber,problemType,problemDescription,fiscalCodeNumber))";
+        private static final String SQLInsert = "INSERT INTO report (reportTicketNumber,problemType,problemDescription,fiscalCodeNumber) VALUE('?','?';'?','?')";
         private static final String SQLUpdate = "UPDATE report SET problemType='?',problemDescription='?' WHERE reportTicketNumber='?'";
-        private static final String SQLDelete = "DELETE FROM report WHERE reportTickerNumber='?'";
+        private static final String SQLDelete = "DELETE FROM report WHERE reportTicketNumber='?'";
 
 
 }
