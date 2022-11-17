29f4724c579d94683b4a978b3700ed7f0975436c
Matteo Golini <matteo.golini@aesys.tech>
2022-11-17 10:57:06 +0100

    prepared statement first

--- BlackBook/src/main/java/it/aesys/courses/springboot/dao/ReportDao.java
+++ BlackBook/src/main/java/it/aesys/courses/springboot/dao/ReportDao.java
@@ -1,5 +1,14 @@
 package it.aesys.courses.springboot.dao;
+import org.springframework.stereotype.Component;
 
-public class ReportDao {
+import java.sql.*;
+@Component
+public class ReportDao{
+        private static final String DRIVER_NAME = " da mettere ";
+        private static final String SQLSelect = "SELECT * FROM report WHERE reportTicketNumber=?";
+        private static final String SQLInsert = "INSERT INTO report (reportTickerNumber,problemType,problemDescription,fiscalCodeNumber))";
+        private static final String SQLUpdate = "UPDATE report SET problemType='?',problemDescription='?' WHERE reportTicketNumber='?'";
+        private static final String SQLDelete = "DELETE FROM report WHERE reportTickerNumber='?'";
+
 
 }
