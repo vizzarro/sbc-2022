f716ebad95312e2415ae079185b35d297ff185ad
LAPTOP-TUGE21QO\Luca Brandolini <luca.brandolini@aesys.tech>
2022-11-17 17:18:14 +0100

    person Service create

--- PersonRegistry/src/main/java/it/aesys/courses/springboot/personregistry/service/PersonService.java
+++ PersonRegistry/src/main/java/it/aesys/courses/springboot/personregistry/service/PersonService.java
@@ -1,12 +1,13 @@
 package it.aesys.courses.springboot.personregistry.service;
 
+import it.aesys.courses.springboot.personregistry.dao.exception.DaoException;
 import it.aesys.courses.springboot.personregistry.models.PersonDTO;
 import it.aesys.courses.springboot.personregistry.service.exceptions.ServiceException;
 
 import java.util.Collection;
 
 public interface PersonService {
-    public PersonDTO create(PersonDTO personDto) throws ServiceException;
+    public PersonDTO create(PersonDTO personDto) throws ServiceException, DaoException;
     public PersonDTO get(String fiscalcode) throws ServiceException;
     public Collection<PersonDTO> getAll() throws ServiceException;
     public PersonDTO update(String fiscalcode, PersonDTO personDTO) throws ServiceException;
